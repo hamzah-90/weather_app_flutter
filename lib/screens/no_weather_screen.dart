1472b5e1a1fc3fb3ed73030991a0e2b75cc25324
HAMZAH <hamzahilmyopen2020@gmail.com>
2025-11-10 16:53:24 +0800

    wow

---
+++ lib/screens/no_weather_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NoWeatherView extends StatelessWidget {
+  const NoWeatherView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
